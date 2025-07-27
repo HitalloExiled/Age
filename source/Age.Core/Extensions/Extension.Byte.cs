4b556031b666738fdc8f176716bb0b0f7bf758b4
Hitallo Exiled <hitalloexiled@hotmail.com>
2025-07-27 17:21:07 -0300

    feat: adds Unsafe.AsReadOnly

--- source/Age.Core/Extensions/Extension.Byte.cs
+++ source/Age.Core/Extensions/Extension.Byte.cs
@@ -8,12 +8,19 @@ public static partial class Extension
 {
     extension(byte[] source)
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public string ConvertToString() =>
             Encoding.Default.GetString(source);
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public string ConvertToString(Encoding encoding) =>
             encoding.GetString(source);
+    }
+
+    extension<T>(Unsafe) where T : unmanaged, allows ref struct
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe ref readonly TTo AsReadOnly<TTo>(scoped in T value) where TTo : unmanaged, allows ref struct =>
+            ref Unsafe.AsRef<TTo>((TTo*)Unsafe.AsPointer(in value));
     }
 }
