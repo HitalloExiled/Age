c3562037c4d011d1d584bb0cc3416953afb230f2
Hitallo Exiled <hitalloexiled@hotmail.com>
2025-09-25 15:25:11 -0300

    wip: implementing render pass

---
+++ source/Age/Graphs/RenderPass.cs
@@ -0,0 +1,18 @@
+using Age.Resources;
+
+namespace Age.Graphs;
+
+public abstract class RenderPass<TOutput> : RenderGraphNode<TOutput>
+where TOutput : new()
+{
+    protected abstract RenderTarget RenderTarget { get; }
+
+    protected sealed override void Execute(RenderContext context) => throw new NotImplementedException();
+}
+
+public abstract class RenderPass<TInput, TOutput> : RenderGraphNode<TInput, TOutput>
+where TInput  : new()
+where TOutput : new()
+{
+    protected sealed override void Execute(RenderContext context) => throw new NotImplementedException();
+}
